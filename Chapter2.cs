5b960b55d07abaed9ae9be581d2ce07843de9d7b
Haiki <106439461+nourredinejinnane20@users.noreply.github.com>
2022-09-18 00:01:50 +0300

    Create Chapter2.cs

---
+++ Chapter2.cs
@@ -0,0 +1,13 @@
+// This program will capture the user number and it will tell the user if the number is positive or negative, if the number is equal to zero the program will write "Got 0", and if the number is greater than 1000 it will write big number
+
+Console.WriteLine("Please input a number: ");
+int number = int.Parse(Console.ReadLine());
+if (number > 0 && number < 1000) 
+{
+    Console.WriteLine("Positive number.");
+} else if (number > 1000)
+{
+    Console.WriteLine("Big number");
+} else if (number < 0)
+{
+    Console.WriteLine("Negative number.");
