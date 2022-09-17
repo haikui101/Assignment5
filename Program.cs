349dc14ac86bae57b0985fbd58b2caf768c94fed
Haiki <106439461+nourredinejinnane20@users.noreply.github.com>
2022-09-17 23:52:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Hey, Please input the number of \nthe weekly hours you have been training basketball: ");
+int weeklyHours = int.Parse(Console.ReadLine());
+int hoursneeded = 30 - weeklyHours;
+
+if (weeklyHours < 30)
+{
+    Console.WriteLine("You cannot participate in the tournament league");
+    Console.WriteLine("Your total traning hours are: " + weeklyHours + ", To participate in the tournament league you need to train " + hoursneeded + " more hours");
+}
+else if (weeklyHours >= 30)
+{
+    Console.WriteLine("You can participate in the tournament league");
+}
